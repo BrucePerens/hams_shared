038bc52d919b2c7eb2b9d7b3e6811e0896ea0e2e
BrucePerens <bruce@perens.com>
2026-07-12 07:18:09 -0700

    Auto: Clean up one-shot debug and migration scripts from tools directory

--- tools/test_datetime_add.py
+++
@@ -1,9 +0,0 @@
-# This software is distributed under the terms of the Affero General Public License (AGPL-3).
-
-from odoo import fields
-
-def run_test():
-    print(hasattr(fields.Datetime, 'add'))
-
-if __name__ == '__main__':
-    run_test()
